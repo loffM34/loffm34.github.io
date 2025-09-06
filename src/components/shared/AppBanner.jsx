96e2b5b73bf385671565383df2f77551639c1176
Michael <loffm300334@gmail.com>
2025-09-06 10:26:19 -0400

    updated resume

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -53,7 +53,7 @@ const AppBanner = () => {
         >
           <a
             download="MichaelLoffResume.pdf"
-            href="/files/MichaelLoffResume2.pdf"
+            href="/files/MichaelLoff.pdf"
             className="font-general-medium flex justify-center items-center w-36 sm:w-48 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
             aria-label="Download Resume"
           >
@@ -63,7 +63,7 @@ const AppBanner = () => {
             </p>
           </a>
           <a
-            href="/files/MichaelLoffResume2.pdf"
+            href="/files/MichaelLoff.pdf"
             target="blank"
             className="font-general-medium flex justify-center items-center max-w-sm sm:w-24 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
             aria-label="Download Resume"
